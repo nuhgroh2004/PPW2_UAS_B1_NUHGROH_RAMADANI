857d5279ee224151b560ae3e9b50ad694ff6e2b4
nuhgroh2004 <nuhgrohramadani@mail.ugm.ac.id>
2024-12-03 14:44:25 +0700

    bugfix #24

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,5 @@
-
+@extends('layouts')
+@section('content')
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
 
@@ -38,3 +39,4 @@
         </div>
     </div>
 </div>
+@endsection
